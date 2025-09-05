aa5e47e89a64279f2288b1daddab2a81b99f662e
Ann <krisjulom@gmail.com>
2025-09-05 14:50:33 +0800

    Add files via upload

---
+++ Constructor_with_Simple-Inheritance/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ybañez_InheritanceWithConstructor
+{
+    class Student //Base Class
+    {
+        //Setting property values for Student class
+        public string Name;
+        public string Program;
+        public Student(string identity, string major) //Constructor
+        {
+            Name = identity;
+            Program = major;
+        }
+        //Method to display Student information
+        public void BasicInfo()
+        {
+            Console.WriteLine("\n------Student Information-----\n");
+            Console.WriteLine($"Name: {Name}    \nProgram: {Program}");
+        }
+    }
+
+    class RegularStudent : Student //Derived Class
+    {
+        //Setting property values for the RegularStudent class
+        public string Section;
+        public RegularStudent(string identity, string major, string classSec) : base(identity, major) //Constructor
+        {
+            Section = classSec;
+        }
+        //Method to display Regular Student information
+        public void SectionEnrolled()
+        {
+            BasicInfo();
+            Console.WriteLine($"Section: {Section}");
+        }
+    }
+
+    class IrregularStudent : Student //Derived Class
+    {
+        //Setting property values for the IrregularStudent class
+        public int UnitsEnrolled;
+        public IrregularStudent(string identity, string major, int enrolledUnits) : base(identity, major) //Constructor
+        {
+            UnitsEnrolled = enrolledUnits;
+        }
+        //Method to display Irregular Student information
+        public void EnrolledSemUnits()
+        {
+            BasicInfo();
+            Console.WriteLine($"Enrolled Units: {UnitsEnrolled}");
+        }
+    }
+}
