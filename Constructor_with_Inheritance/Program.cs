436831db2463ef13815306152e2a4b8cc14857d4
アンネ <krisjulom@gmail.com>
2025-11-20 16:41:08 +0800

    Add files via upload

---
+++ Constructor_with_Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_with_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name: Kris Anne J. Ybañez
+            //Section: IT306
+            //Title: Simple Inheritance with Constructor
+
+            //Instantiate an object for the Student Class
+            Student student1 = new Student("Aliza", "IT");
+            student1.BasicInfo(); //Call method to display BasicInfo
+
+            RegularStudent student2 = new RegularStudent("Amira", "IT", "IT103P");
+            student2.SectionEnrolled(); //Call method to display SectionEnrolled
+
+            IrregularStudent student3 = new IrregularStudent("Amira", "IT", 21);
+            student3.EnrolledSemUnits(); //Call method to display UnitsEnrolled
+            Console.ReadKey(); //End =)
+        }
+    }
+}
