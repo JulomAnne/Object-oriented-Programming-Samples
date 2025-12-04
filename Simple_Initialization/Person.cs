c356f91fe2dd6a109c4a64c15755d90024cef76f
アンネ <krisjulom@gmail.com>
2025-12-04 16:55:20 +0800

    Add files via upload

---
+++ Simple_Initialization/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Initialization
+{
+    internal class Person
+    {
+        //Class Members
+        public string name, gender;
+        public int age;
+
+        //Method to display
+        public void Greet()
+        {
+            Console.WriteLine("Hello. I'm " + name + " and I'm " + age + " years old. I'm a " + gender);
+        }
+    }
+}
---
+++ Simple_Initialization/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Initialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kris Anne J. Ybañez   IT306   EXER_01: Creating a Class
+
+            Person person1 = new Person();
+
+            //Initialization of fields
+            person1.name = "Kris Anne";
+            person1.age = 16;
+            person1.gender = "Girl";
+            person1.Greet();
+            Console.ReadLine();
+        }
+    }
+}
