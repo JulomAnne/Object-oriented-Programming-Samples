9827b5cb7c3e1cfe8e7f7e136ec913d94fc237e6
アンネ <krisjulom@gmail.com>
2026-01-15 17:12:56 +0800

    Add files via upload

---
+++ Polymorphism-Parameterized_Constructor/Bag.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Parameterized_Constructor
+{
+    public class Bag //Base Class
+    {
+        //Bag Class Properties
+        public string Brand { get; set; }
+        public double Price { get; set; }
+
+        //Parameterized Constructor
+        public Bag(string brand, double price)
+        {
+            Brand = brand;
+            Price = price;
+        }
+
+        //Virtual Method
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Price: ₱{Price}");
+        }
+    }
+
+    public class SchoolBag : Bag //Derived Class
+    {
+        public int Compartments { get; set; } //SchoolBag Class Property
+
+        //Parameterized Constructor
+        public SchoolBag(string brand, double price, int compartments) : base(brand, price)
+        {
+            Compartments = compartments;
+        }
+
+        //Override Method
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("--- School Bag ---");
+            base.DisplayInfo();
+            Console.WriteLine($"Compartments: {Compartments}");
+            Console.WriteLine();
+        }
+    }
+
+    public class TravelBag : Bag //Derived Class
+    {
+        public int Capacity { get; set; } //TravelBag Class Property
+
+        //Parameterized Constructor
+        public TravelBag(string brand, double price, int capacity) : base(brand, price)
+        {
+            Capacity = capacity;
+        }
+
+        // Override DisplayInfo()
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("--- Travel Bag ---");
+            base.DisplayInfo();
+            Console.WriteLine($"Capacity: {Capacity} liters");
+            Console.WriteLine();
+        }
+    }
+}
