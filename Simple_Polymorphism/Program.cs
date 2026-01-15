57937fc577e824007f8ffe65970c29a735a3934c
アンネ <krisjulom@gmail.com>
2026-01-15 17:07:34 +0800

    Add files via upload

---
+++ Simple_Polymorphism/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Insttantiate an object
+            Student normal = new Student();
+            Irregular person = new Irregular();
+            Working scholar = new Working();
+
+            //Method of display
+            normal.ComputeTuitionFee();
+            person.ComputeTuitionFee();
+            scholar.ComputeTuitionFee();
+            Console.ReadLine();
+        }
+    }
+}
