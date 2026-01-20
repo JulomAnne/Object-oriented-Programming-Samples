a0f3a27c5401259c3bb7ffb73481625a127ca739
アンネ <krisjulom@gmail.com>
2026-01-20 13:22:13 +0800

    Add files via upload

---
+++ Abstraction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    //Name: Kris Anne J. Ybanez
+    //Section: IT306P
+    //Subject: OOPROG
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            //Instantiate derived object class
+            SchoolBag backpack = new SchoolBag("JanSport", "Blue", 1500, 5);
+            TravelBag travelBag = new TravelBag("Samsonite", "Black", 4500, 60);
+
+            //Display details & discount examples
+            backpack.DisplayDetails();
+            backpack.CalculateDiscount(10);
+            backpack.Carry();
+
+            travelBag.DisplayDetails();
+            travelBag.CalculateDiscount(15);
+            travelBag.Carry();
+
+            Console.ReadKey();
+        }
+    }
+}
