561721f166d8308f2e769c69e2d090a24ba80fbf
アンネ <krisjulom@gmail.com>
2026-01-20 13:15:32 +0800

    Add files via upload

---
+++ User-Input_with_Inheritance/Bag.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input_with_Inheritance
+{
+    class Bag //Base Class
+    {
+        //Declaring fields
+        public string Style { get; set; }
+        public string Color { get; set; }
+
+        public virtual void DisplayInfo() //Method of display
+        {
+            Console.WriteLine($"Name: {Style} \nColor: {Color}\n");
+            Console.WriteLine($"This is a {Color} {Style}!");
+        }
+    }
+
+    class HandBag : Bag //Derived Class
+    {
+        public HandBag(string name, string color) //Constructor
+        {
+            Style = name;
+            Color = color;
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Style} \nColor: {Color}\n");
+            Console.WriteLine($"This is a {Color} {Style}!");
+        }
+    }
+
+    class ShoulderBag : Bag //Derived Class
+    {
+        public ShoulderBag(string name, string color) //Constructor
+        {
+            Style = name;
+            Color = color;
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Style} \nColor: {Color}\n");
+            Console.WriteLine($"This is a {Color} {Style}!");
+        }
+    }
+}
